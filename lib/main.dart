6d4ed66ba31989ff133df0c353d04c1a7235620e
Mate Narh <spartaninzaghi@gmail.com>
2026-02-16 15:31:38 -0500

    converted GamePage to a StatefulWidget and made app respond to user input with setState

--- lib/main.dart
+++ lib/main.dart
@@ -55,36 +55,46 @@ class Tile extends StatelessWidget {
   }
 }
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   GamePage({super.key});
-  // This manages game logic, and is out of scope for this lesson.
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
-            for (var guess in _game.guesses)
-              Row(
-                spacing: 5.0,
-                children: [
-                  for (var letter in guess)
-                    Tile(letter.char, letter.type),
-                ]
-              ),
-            GuessInput(
-              onSubmitGuess: (String guess) {
-                // TODO: handle guess
-                print(guess); // temmporary
-              }
+          for (var guess in _game.guesses)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
+              ],
             ),
-          ],
+          GuessInput(
+           onSubmitGuess: (String guess) {
+              setState(() { // NEW
+                _game.guess(guess);
+              });
+            },
+          ),
+        ],
       ),
     );
   }
 }
+
 
 
 class GuessInput extends StatelessWidget {
